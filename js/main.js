645abe43363e260722d8c9e69cd88204c6039572
Cael Santana <cael2@hotmail.com>
2023-01-17 20:53:14 -0300

    Atualizações de layout, imagem e responsividade

--- js/main.js
+++ js/main.js
@@ -2,3 +2,8 @@
 function slideOut() {
   document.getElementById('check').checked = false;
 }
+
+// DARK-MODE
+document.querySelector("#btn").addEventListener("click", () => {
+  document.body.classList.toggle("dark-mode")
+})
